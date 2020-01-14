a05b6fd4114cd7157c7b8f77f1376f256bea3d37
Miguel Verissimo <m.verissimo@gmail.com>
2020-01-13 21:02:21 -0500

    Clean up zshrc to play with prezto
    Also: create stub for install script

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+CONFIGS_DIR=$(cd "$(dirname "$0")"; pwd)
+
+echo "clone prezto"
+# git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+
+ROOT_FILES="zshrc"
+
+for CONFIG_FILE in $ROOT_FILES; do
+  if [ -e ${HOME}/.${CONFIG_FILE} ]; then
+    echo "Backing current ${HOME}/.${CONFIG_FILE} to ${HOME}/.${CONFIG_FILE}.prev"
+    mv ${HOME}/.${CONFIG_FILE}{,.prev}
+  fi
+
+  echo "creating ${HOME}/.${CONFIG_FILE} symlinked to ${CONFIGS_DIR}/${CONFIG_FILE}"
+  ln -s ${CONFIGS_DIR}/${CONFIG_FILE} ${HOME}/.${CONFIG_FILE}
+done
+
+echo "Now run all the symlinking for the supported software"
+# for every directory, run the config.sh
