dd7f255711603792b55715287c5826ad371a802a
Miguel Verissimo <m.verissimo@gmail.com>
2020-05-31 00:10:33 -0400

    Add RUBYOPTS to silence ruby depracation warnings

--- zsh/ruby.zsh
+++ zsh/ruby.zsh
@@ -1,3 +1,6 @@
+# Get rid of the pesky deprecation warnings since ruby 2.7.0
+export RUBYOPT='-W:no-deprecated -W:no-experimental'
+
 # Ruby and Rails related
 alias b="bundle"
 alias bu="bundle update"
