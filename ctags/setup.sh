7f147b2ca15fa3d207fe2938e8abaf9808906d77
Miguel Verissimo <m.verissimo@gmail.com>
2020-07-18 13:34:20 -0400

    Fix typo in ctags install script

--- ctags/setup.sh
+++ ctags/setup.sh
@@ -5,7 +5,7 @@ set -e
 source ../_setup_scripts/backup_and_symlink
 
 CONFIGS_DIR=$(cd "$(dirname "$0")"; pwd)
-ORIGIN="${CONFIGS_DIR}/ctags/ctags"
+ORIGIN="${CONFIGS_DIR}/ctags"
 DEST="${HOME}/.ctags"
 
 backup_and_symlink ${ORIGIN} ${DEST}
